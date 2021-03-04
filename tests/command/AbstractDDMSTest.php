a798e5b5c017d3c82c989fb8a29bc1fde73d72db
sevidmusic <sdmwwebsdm@gmail.com>
2021-03-03 23:28:58 -0500

    ddms\abstractions\AbstractDDMS: Minor refactoring of tests\command\AbstractDDMSTest. All PhpUnit and PhpStan tests are passing.

--- tests/command/AbstractDDMSTest.php
+++ tests/command/AbstractDDMSTest.php
@@ -12,16 +12,20 @@ final class AbstractDDMSTest extends TestCase
     public function testRunCommandReturnsValueMatchingValueReturnedOnIndependantCallToSpecifiedCommandsRunMethod(): void
     {
 
+        $mockUserInterface = $this->getMockBuilder(DDMSUserInterface::class)
+            ->getMock();
+
         $mockCommand = $this->getMockBuilder(DDMSCommand::class)
             ->setMethods(['run'])
             ->getMock();
 
-        $mockDDMS = $this->getMockBuilder(DDMS::class)
-            ->setMethods(['run'])
-            ->getMock();
+        $mockCommand->method('run')
+            ->willReturn(true);
 
-        $mockUserInterface = $this->getMockBuilder(DDMSUserInterface::class)
-            ->getMock();
+        $mockDDMS = $this->getMockForAbstractClass(DDMS::class);
+        $mockDDMS->expects($this->any())->method('run')
+            ->will($this->returnValue(false)
+        );
 
         $this->assertEquals(
             $mockCommand->run($mockUserInterface, $mockCommand->prepareArguments([])),
