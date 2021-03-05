de971d211fbd90304e7922f84229c339f808f69a
sevidmusic <sdmwwebsdm@gmail.com>
2021-03-05 17:32:17 -0500

    bin/ddms.php: Code clean up.

--- bin/ddms.php
+++ bin/ddms.php
@@ -11,21 +11,15 @@ $ui = new UserInterface();
 $commandFactory = new CommandFactory();
 $help = new Help();
 $ddms = new DDMS();
-
-$banner = "
-  \e[0m\e[94m    _    _\e[0m
-  \e[0m\e[93m __| |__| |_ __  ___\e[0m
-  \e[0m\e[94m/ _` / _` | '  \(_-<\e[0m
-  \e[0m\e[91m\__,_\__,_|_|_|_/__/\e[0m
-  \e[0m\e[105m\e[30m  v0.0.1  \e[0m\e[0m\e[101m\e[30m  " . date('h:i:s A') . "  \e[0m
-
-";
-
-$ui->showMessage($banner);
-
-$ddms->runCommand($ui, $help, $argv);
-
 $arguments = $ddms->prepareArguments($argv);
+
+$ui->showMessage(getBanner());
+
+try {
+    $ddms->run($ui, $ddms->prepareArguments($argv));
+} catch(\RuntimeException $ddmsError) {
+    $ui->showMessage(getDevInvalidCommandMsg($ddmsError));
+}
 
 if(in_array('DEBUGOPTIONS' ,$arguments['options'])) {
     showOptions($ui, $arguments);
@@ -34,13 +28,6 @@ if(in_array('DEBUGOPTIONS' ,$arguments['options'])) {
 if(in_array('DEBUGFLAGS' ,$arguments['options'])) {
     showFlags($ui, $arguments);
 }
-
-try {
-    $ddms->run($ui, $ddms->prepareArguments($argv));
-} catch(\RuntimeException $ddmsError) {
-    $ui->showMessage(PHP_EOL . "\e[0m  \e[103m\e[30m" . str_replace(['Error', 'ddms --help'], ["\e[0m\e[102m\e[30mError\e[0m\e[103m\e[30m", "\e[0m\e[104m\e[30mddms --help\e[0m\e[103m\e[30m"], $ddmsError->getMessage()) . "\e[0m" . PHP_EOL);
-}
-
 
 /**
  * Dev Functions
@@ -73,4 +60,33 @@ function showFlags(UserInterface $ui, array $arguments): void
         }
         $ui->showMessage(PHP_EOL);
     }
+}
+
+function getBanner():string
+{
+    return
+    $banner = "
+  \e[0m\e[94m    _    _\e[0m
+  \e[0m\e[93m __| |__| |_ __  ___\e[0m
+  \e[0m\e[94m/ _` / _` | '  \(_-<\e[0m
+  \e[0m\e[91m\__,_\__,_|_|_|_/__/\e[0m
+  \e[0m\e[105m\e[30m  v0.0.1  \e[0m\e[0m\e[101m\e[30m  " . date('h:i:s A') . "  \e[0m";
+
+}
+
+function getDevInvalidCommandMsg(\RuntimeException $ddmsError): string
+{
+    return PHP_EOL .
+            "\e[0m  \e[103m\e[30m" .
+            str_replace(
+                [
+                    'Error',
+                    'ddms --help'
+                ],
+                [
+                    "\e[0m\e[102m\e[30mError\e[0m\e[103m\e[30m",
+                    "\e[0m\e[104m\e[30mddms --help\e[0m\e[103m\e[30m"
+                ],
+                $ddmsError->getMessage()
+            ) . "\e[0m" . PHP_EOL;
 }
