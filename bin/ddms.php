a99061389199f3276df99d56dd3151cfcfc847be
sevidmusic <sdmwwebsdm@gmail.com>
2021-03-06 22:59:34 -0500

    ddms\classes\ui\CommandLineUI: Added new method, showBanner(). Implemented testShowBannerOutputsDDMSBanner(). All PhpUnit tests are passing, all PhpStan tests are passing.

--- bin/ddms.php
+++ bin/ddms.php
@@ -13,7 +13,7 @@ $help = new Help();
 $ddms = new DDMS($commandFactory);
 $arguments = $ddms->prepareArguments($argv);
 
-$ui->showMessage(getBanner());
+$ui->showBanner();
 
 try {
     $ddms->run($ui, $ddms->prepareArguments($argv));
@@ -60,18 +60,6 @@ function showFlags(UserInterface $ui, array $arguments): void
         }
         $ui->showMessage(PHP_EOL);
     }
-}
-
-function getBanner():string
-{
-    return
-    $banner = "
-  \e[0m\e[94m    _    _\e[0m
-  \e[0m\e[93m __| |__| |_ __  ___\e[0m
-  \e[0m\e[94m/ _` / _` | '  \(_-<\e[0m
-  \e[0m\e[91m\__,_\__,_|_|_|_/__/\e[0m
-  \e[0m\e[105m\e[30m  v0.0.1  \e[0m\e[0m\e[101m\e[30m  " . date('h:i:s A') . "  \e[0m";
-
 }
 
 function getDevInvalidCommandMsg(\RuntimeException $ddmsError): string
