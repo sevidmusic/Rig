e9bdbba29cbcf02245180096a135d66e8d74bf87
sevidmusic <sdmwwebsdm@gmail.com>
2021-03-18 10:56:52 -0400

    Code clean up.

--- ddms/classes/command/NewGlobalResponse.php
+++ ddms/classes/command/NewGlobalResponse.php
@@ -12,6 +12,27 @@ class NewGlobalResponse extends AbstractCommand implements Command
 
     public function run(UserInterface $userInterface, array $preparedArguments = ['flags' => [], 'options' => []]): bool
     {
+        $this->validateArguments($preparedArguments);
+        ['flags' => $flags] = $preparedArguments;
+        if(!file_exists($this->determineAppDirectoryPath($flags)) || !is_dir($this->determineAppDirectoryPath($flags))) {
+            throw new RuntimeException('  An App does not exist at' . $this->determineAppDirectoryPath($flags));
+        }
+        return true;
+    }
+
+    /**
+     * @param array <string, array<int, string>> $flags
+     */
+    private function determineAppDirectoryPath(array $flags): string
+    {
+        return  $flags['ddms-apps-directory-path'][0] . DIRECTORY_SEPARATOR . $flags['for-app'][0];
+    }
+
+    /**
+     * @param array{"flags": array<string, array<int, string>>, "options": array<int, string>} $preparedArguments
+     */
+    private function validateArguments(array $preparedArguments): void
+    {
         ['flags' => $flags] = $preparedArguments;
         if(!isset($flags['name'][0])) {
             throw new RuntimeException('  Please specify a name for the new GlobalResponse.');
@@ -19,11 +40,6 @@ class NewGlobalResponse extends AbstractCommand implements Command
         if(!isset($flags['for-app'][0])) {
             throw new RuntimeException('  Please specify the name of the App to create the new GlobalResponse for');
         }
-        $appDirectoryPath = $flags['ddms-apps-directory-path'][0] . DIRECTORY_SEPARATOR . $flags['for-app'][0];
-        if(!file_exists($appDirectoryPath) || !is_dir($appDirectoryPath)) {
-            throw new RuntimeException('  An App does not exist at' . $appDirectoryPath);
-        }
-        return true;
     }
 
 }
