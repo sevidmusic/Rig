e55001a41f4477c66614931ca4410cbde2626f36
sevidmusic <sdmwwebsdm@gmail.com>
2021-03-20 20:39:58 -0400

    ddms\classes\command\NewDynamicOutputComponent: Began development of ddms\classes\command\NewDynamicOutputComponent. Implemented the following tests:
         testRunThrowsRuntimeExceptionIf_name_IsNotSpecified()
         testRunThrowsRuntimeExceptionIf_for_app_IsNotSpecified()
         testRunThrowsRuntimeExceptionIfSpecifiedAppDoesNotExist()
         testRunCreatesNewDynamicOutputComponentForSpecifiedApp()
         testRunSetsContainerTo_DynamicOutputComponents_IfContainerIsNotSpecified()
         testRunSetsContainerTo_DynamicOutputComponents_IfContainerIsSpecifiedWithNoValue()
         testRunThrowsRuntimeExceptionIfSpecifiedContainerIsNotAlphaNumeric()
         testRunSetsContainerToSpecifiedContainerIfSpecifiedContainerIsAlphaNumeric()
         testRunSetsPositionTo_0_IfPositionIsNotSpecified()
         testRunSetsPositionTo_0_IfPositionIsSpecifiedWithNoValue()
         testRunThrowsRuntimeExceptionIfSpecifiedPositionIsNotNumeric()
         testRunSetsPositionToSpecifiedPositionIfSpecifiedPositionIsNumeric()
         testRunThrowsRuntimeExceptionIfDynamicOutputComponentAlreadyExists()
         testRunThrowsRuntimeExpceptionIfSpecifiedNameIsNotAlphaNumeric()
         testRunSetsNameToSpecifiedNameIfSpecifiedNameIsAlphaNumeric()
    
    All PhpUnit and PhpStan tests are passing.
    
    This begins to addres issue #12.

---
+++ ddms/classes/command/NewDynamicOutputComponent.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace ddms\classes\command;
+
+use ddms\interfaces\command\Command;
+use ddms\abstractions\command\AbstractCommand;
+use ddms\interfaces\ui\UserInterface;
+use \RuntimeException;
+
+class NewDynamicOutputComponent extends AbstractCommand implements Command
+{
+
+    public function run(UserInterface $userInterface, array $preparedArguments = ['flags' => [], 'options' => []]): bool
+    {
+        ['flags' => $flags] = $this->validateArguments($preparedArguments);
+        return boolval(
+            file_put_contents(
+                $this->pathToNewDynamicOutputComponent($flags),
+                $this->generateDynamicOutputComponentFileContent($flags)
+            )
+        );
+    }
+
+    /**
+     * @param array<string, array<int, string>> $flags
+     */
+    private function generateDynamicOutputComponentFileContent(array $flags): string
+    {
+        $template = strval(file_get_contents($this->pathToDynamicOutputComponentTemplate()));
+         return str_replace(
+            [
+                '_NAME_',
+                '_POSITION_',
+                '_CONTAINER_'
+            ],
+            [
+                $flags['name'][0],
+                ($flags['position'][0] ?? '0'),
+                ($flags['container'][0] ?? 'DynamicOutputComponents'),
+            ],
+            $template
+        );
+    }
+
+    private function pathToDynamicOutputComponentTemplate(): string
+    {
+        $templatePath = str_replace('ddms' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'command', 'FileTemplates', __DIR__) . DIRECTORY_SEPARATOR . 'DynamicOutputComponent.php';
+        if(!file_exists($templatePath)) {
+            throw new RuntimeException('Error: The DynamicOutputComponent.php file template is missing! You will not be able to create new DynamicOutputComponents until the DynamicOutputComponent.php template is restored at FileTemplates/DynamicOutputComponent.php');
+        }
+        return $templatePath;
+
+    }
+
+    /**
+     * @param array<string,array<int,string>> $flags
+     */
+    private function pathToNewDynamicOutputComponent(array $flags): string
+    {
+        return $flags['ddms-apps-directory-path'][0] . DIRECTORY_SEPARATOR . $flags['for-app'][0] . DIRECTORY_SEPARATOR . 'OutputComponents' . DIRECTORY_SEPARATOR . $flags['name'][0] . '.php';
+    }
+
+    /**
+     * @param array{"flags": array<string, array<int, string>>, "options": array<int, string>} $preparedArguments
+     * @return array{"flags": array<string, array<int, string>>, "options": array<int, string>}
+     */
+    private function validateArguments(array $preparedArguments): array
+    {
+        ['flags' => $flags] = $preparedArguments;
+        if(!isset($flags['name'][0])) {
+            throw new RuntimeException('  Please specify a name for the new DynamicOutputComponent.');
+        }
+        if(!ctype_alnum($flags['name'][0])) {
+            throw new RuntimeException('  Please specify an alphanumeric name for the new DynamicOutputComponent.');
+        }
+        if(isset($flags['container'][0]) && !ctype_alnum($flags['container'][0])) {
+            throw new RuntimeException('  Please specify an alphanumeric name for the new DynamicOutputComponent.');
+        }
+        if(!isset($flags['for-app'][0])) {
+            throw new RuntimeException('  Please specify the name of the App to create the new DynamicOutputComponent for');
+        }
+        if(!file_exists($this->determineAppDirectoryPath($flags)) || !is_dir($this->determineAppDirectoryPath($flags))) {
+            throw new RuntimeException('  An App does not exist at' . $this->determineAppDirectoryPath($flags));
+        }
+        if(file_exists($this->pathToNewDynamicOutputComponent($flags))) {
+            throw new RuntimeException('  Please specify a unique name for the new DynamicOutputComponent');
+        }
+        if(isset($flags['position'][0]) && !is_numeric($flags['position'][0])) {
+            throw new RuntimeException('  Please specify a numeric position for the new DynamicOutputComponent. For example `--position 1`.');
+        }
+        return $preparedArguments;
+    }
+
+    /**
+     * @param array <string, array<int, string>> $flags
+     */
+    private function determineAppDirectoryPath(array $flags): string
+    {
+        return  $flags['ddms-apps-directory-path'][0] . DIRECTORY_SEPARATOR . $flags['for-app'][0];
+    }
+
+
+}
---
+++ tests/command/NewDynamicOutputComponentTest.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace tests\command;
+
+use PHPUnit\Framework\TestCase;
+use ddms\classes\command\NewApp;
+use ddms\classes\command\NewDynamicOutputComponent;
+use ddms\classes\ui\CommandLineUI;
+use ddms\interfaces\ui\UserInterface;
+use \RuntimeException;
+use tests\traits\TestsCreateApps;
+
+final class NewDynamicOutputComponentTest extends TestCase
+{
+
+    use TestsCreateApps;
+
+    public function testTest(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testRunThrowsRuntimeExceptionIf_name_IsNotSpecified(): void
+    {
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $this->expectException(RuntimeException::class);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $newDynamicOutputComponent->prepareArguments([]));
+    }
+
+    public function testRunThrowsRuntimeExceptionIf_for_app_IsNotSpecified(): void
+    {
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $this->expectException(RuntimeException::class);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $newDynamicOutputComponent->prepareArguments(['--name', 'Foo']));
+    }
+
+    public function testRunThrowsRuntimeExceptionIfSpecifiedAppDoesNotExist(): void
+    {
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $this->expectException(RuntimeException::class);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $newDynamicOutputComponent->prepareArguments(['--name', 'Foo', '--for-app', 'Baz' . strval(rand(10000,9999))]));
+    }
+
+    public function testRunCreatesNewDynamicOutputComponentForSpecifiedApp(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $preparedArguments = $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName]);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+        $this->assertTrue(file_exists($this->expectedDynamicOutputComponentPath($preparedArguments)));
+    }
+
+    public function testRunSetsContainerTo_DynamicOutputComponents_IfContainerIsNotSpecified(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $preparedArguments = $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName]);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+        $this->assertEquals($this->determineExpectedDynamicOutputComponentPhpContent($preparedArguments), $this->getNewDynamicOutputComponentContent($preparedArguments));
+    }
+
+    public function testRunSetsContainerTo_DynamicOutputComponents_IfContainerIsSpecifiedWithNoValue(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $preparedArguments = $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName, '--container']);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+        $this->assertEquals($this->determineExpectedDynamicOutputComponentPhpContent($preparedArguments), file_get_contents($this->expectedDynamicOutputComponentPath($preparedArguments)));
+    }
+
+    public function testRunThrowsRuntimeExceptionIfSpecifiedContainerIsNotAlphaNumeric(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $this->expectException(RuntimeException::class);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName, '--container', 'FooBarBaz*#$%*']));
+    }
+
+    public function testRunSetsContainerToSpecifiedContainerIfSpecifiedContainerIsAlphaNumeric(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $preparedArguments = $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName, '--container', 'ValidContainerName']);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+        $this->assertEquals($this->determineExpectedDynamicOutputComponentPhpContent($preparedArguments), file_get_contents($this->expectedDynamicOutputComponentPath($preparedArguments)));
+    }
+
+    public function testRunSetsPositionTo_0_IfPositionIsNotSpecified(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $preparedArguments = $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName]);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+        $this->assertEquals($this->determineExpectedDynamicOutputComponentPhpContent($preparedArguments), $this->getNewDynamicOutputComponentContent($preparedArguments));
+    }
+
+    public function testRunSetsPositionTo_0_IfPositionIsSpecifiedWithNoValue(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $preparedArguments = $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName, '--position']);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+        $this->assertEquals($this->determineExpectedDynamicOutputComponentPhpContent($preparedArguments), file_get_contents($this->expectedDynamicOutputComponentPath($preparedArguments)));
+    }
+
+    public function testRunThrowsRuntimeExceptionIfSpecifiedPositionIsNotNumeric(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $this->expectException(RuntimeException::class);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName, '--position', 'FooBarBaz']));
+    }
+
+    public function testRunSetsPositionToSpecifiedPositionIfSpecifiedPositionIsNumeric(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $preparedArguments = $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName, '--position', '420']);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+        $this->assertEquals($this->determineExpectedDynamicOutputComponentPhpContent($preparedArguments), file_get_contents($this->expectedDynamicOutputComponentPath($preparedArguments)));
+    }
+
+    public function testRunThrowsRuntimeExceptionIfDynamicOutputComponentAlreadyExists(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $preparedArguments = $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName]);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+        $this->expectException(RuntimeException::class);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+    }
+
+    public function testRunThrowsRuntimeExpceptionIfSpecifiedNameIsNotAlphaNumeric(): void
+    {
+        #ctype_alnum($string)
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $preparedArguments = $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName . '!@#$%^&*()_+=-\][\';"\\,.', '--for-app', $appName]);
+        $this->expectException(RuntimeException::class);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+    }
+
+    public function testRunSetsNameToSpecifiedNameIfSpecifiedNameIsAlphaNumeric(): void
+    {
+        $appName = $this->createTestAppReturnName();
+        $dynamicOutputComponentName = $appName . 'DynamicOutputComponent';
+        $newDynamicOutputComponent = new NewDynamicOutputComponent();
+        $preparedArguments = $newDynamicOutputComponent->prepareArguments(['--name', $dynamicOutputComponentName, '--for-app', $appName]);
+        $newDynamicOutputComponent->run(new CommandLineUI(), $preparedArguments);
+        $this->assertEquals($this->determineExpectedDynamicOutputComponentPhpContent($preparedArguments), file_get_contents($this->expectedDynamicOutputComponentPath($preparedArguments)));
+    }
+
+    /**
+     * @param array{"flags": array<string, array<int, string>>, "options": array<int, string>} $preparedArguments
+     */
+    private function getNewDynamicOutputComponentContent($preparedArguments): string
+    {
+        return strval(file_get_contents($this->expectedDynamicOutputComponentPath($preparedArguments)));
+    }
+
+    /**
+     * @param array{"flags": array<string, array<int, string>>, "options": array<int, string>} $preparedArguments
+     */
+    private function expectedDynamicOutputComponentPath(array $preparedArguments): string
+    {
+        return self::expectedAppDirectoryPath($preparedArguments) . DIRECTORY_SEPARATOR . 'OutputComponents' . DIRECTORY_SEPARATOR . $preparedArguments['flags']['name'][0] . '.php';
+    }
+
+    private function createTestAppReturnName(): string
+    {
+        $appName = self::getRandomAppName();
+        $newApp = new NewApp();
+        $newAppPreparedArguments = $newApp->prepareArguments(['--name', $appName]);
+        $newApp->run(new CommandLineUI(), $newAppPreparedArguments);
+        return $appName;
+    }
+
+    /**
+     * @param array{"flags": array<string, array<int, string>>, "options": array<int, string>} $preparedArguments
+     */
+    private function determineExpectedDynamicOutputComponentPhpContent(array $preparedArguments): string
+    {
+        return str_replace(
+            [
+                '_NAME_',
+                '_POSITION_',
+                '_CONTAINER_',
+                # '_FILE_NAME_',
+                # '_FOR_APP_',
+            ],
+            [
+                $preparedArguments['flags']['name'][0],
+                ($preparedArguments['flags']['position'][0] ?? '0'),
+                ($preparedArguments['flags']['container'][0] ?? 'DynamicOutputComponents'),
+            ],
+            strval(file_get_contents($this->expectedTemplateFilePath()))
+        );
+    }
+
+    private function expectedTemplateFilePath(): string
+    {
+        return str_replace('tests' . DIRECTORY_SEPARATOR . 'command', 'FileTemplates', __DIR__) . DIRECTORY_SEPARATOR . 'DynamicOutputComponent.php';
+    }
+
+}
