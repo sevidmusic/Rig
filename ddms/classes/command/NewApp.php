c65731640437473b305430418567dcb1df47fd81
sevidmusic <sdmwwebsdm@gmail.com>
2021-03-23 20:41:48 -0400

    ddms\classes\commands: Refactored NewApp, NewDynamicOutputComponent, NewGlobalResponse, and StartServer. Implemented ui messages where appropriate.

--- ddms/classes/command/NewApp.php
+++ ddms/classes/command/NewApp.php
@@ -9,26 +9,48 @@ use \RuntimeException;
 class NewApp extends AbstractCommand implements Command
 {
 
+    /**
+     * @var UserInterface $ui
+     */
+    private $ui;
 
     public function run(UserInterface $userInterface, array $preparedArguments = ['flags' => [], 'options' => []]): bool
     {
+        $this->ui = $userInterface;
         ['flags' => $flags] = $preparedArguments;
         if(!in_array('name', array_keys($flags))) {
             throw new RuntimeException('  You must specify a name for the new App');
         }
-        $appDirectoryPath = $flags['ddms-apps-directory-path'][0] . DIRECTORY_SEPARATOR . $flags['name'][0];
-        if(is_dir($appDirectoryPath)) {
+        if(is_dir($this->pathToAppDirectory($flags))) {
             throw new RuntimeException('An App named ' .  $flags['name'][0] . ' already exists. Please specify a unique name.');
         }
-        $this->createAppsDirectoryStructure($appDirectoryPath);
-        $this->createAppsComponentsPhp($appDirectoryPath, $preparedArguments);
+        $this->createAppsDirectoryStructure($flags);
+        $this->createAppsComponentsPhp($flags);
         return true;
     }
 
     /**
-     * @param array{"flags": array<string, array<int, string>>, "options": array<int, string>} $preparedArguments
+     * @param array<string, array<int, string>> $flags
      */
-    private function createAppsComponentsPhp(string $appDirectoryPath, array $preparedArguments): void
+    private function pathToAppDirectory(array $flags): string
+    {
+        return $flags['ddms-apps-directory-path'][0] . DIRECTORY_SEPARATOR . $flags['name'][0];
+    }
+
+    private function showMessage(string $message): void
+    {
+        $this->ui->showMessage(
+            PHP_EOL .
+            $message .
+            PHP_EOL .
+            PHP_EOL
+        );
+    }
+
+    /**
+     * @param array<string, array<int, string>> $flags
+     */
+    private function createAppsComponentsPhp(array $flags): void
     {
         $componentPhpFileTemplatePath = str_replace(
             'ddms' . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'command',
@@ -36,31 +58,63 @@ class NewApp extends AbstractCommand implements Command
             __DIR__
         );
         $componentsPhpTemplate = strval(file_get_contents($componentPhpFileTemplatePath));
-        $content = str_replace('_DOMAIN_', $this->determineDomain($preparedArguments), $componentsPhpTemplate);
-        file_put_contents($appDirectoryPath . DIRECTORY_SEPARATOR . 'Components.php', $content);
+        $content = str_replace('_DOMAIN_', $this->determineDomain($flags), $componentsPhpTemplate);
+        $componentsPhpPath = $this->pathToAppDirectory($flags) . DIRECTORY_SEPARATOR . 'Components.php';
+        $this->showMessage('Creating new Components.php for App ' . $flags['name'][0] . ' at ' . $componentsPhpPath);
+        file_put_contents($componentsPhpPath, $content);
     }
 
     /**
-     * @param array{"flags": array<string, array<int, string>>, "options": array<int, string>} $preparedArguments
+     * @param array<string, array<int, string>> $flags
      */
-    private function determineDomain(array $preparedArguments): string
+    private function determineDomain(array $flags): string
     {
-        ['flags' => $flags] = $preparedArguments;
         if(isset($flags['domain']) && isset($flags['domain'][0]) && filter_var($flags['domain'][0], FILTER_VALIDATE_URL)) {
             return $flags['domain'][0];
         }
         return 'http://localhost:8080/';
     }
 
-    private function createAppsDirectoryStructure(string $appDirectoryPath): void {
-            mkdir($appDirectoryPath);
-            mkdir($appDirectoryPath . DIRECTORY_SEPARATOR . 'css');
-            mkdir($appDirectoryPath . DIRECTORY_SEPARATOR . 'js');
-            mkdir($appDirectoryPath . DIRECTORY_SEPARATOR . 'DynamicOutput');
-            mkdir($appDirectoryPath . DIRECTORY_SEPARATOR . 'resources');
-            mkdir($appDirectoryPath . DIRECTORY_SEPARATOR . 'Responses');
-            mkdir($appDirectoryPath . DIRECTORY_SEPARATOR . 'Requests');
-            mkdir($appDirectoryPath . DIRECTORY_SEPARATOR . 'OutputComponents');
+    /**
+     * @param array<string, array<int, string>> $flags
+     */
+    private function createAppsDirectoryStructure(array $flags): void {
+            $this->showMessage('Creating new App ' . $flags['name'][0] . ' at ' . $this->pathToAppDirectory($flags));
+            mkdir($this->pathToAppDirectory($flags));
+
+            $this->showMessage('Creating new css directory for App ' . $flags['name'][0] . ' at ' . $this->determineAppSubDirPath('css', $flags));
+            mkdir($this->determineAppSubDirPath('css', $flags));
+
+            $this->showMessage('Creating new js directory for App ' . $flags['name'][0] . ' at ' . $this->determineAppSubDirPath('js', $flags));
+            mkdir($this->determineAppSubDirPath('js', $flags));
+
+            $this->showMessage('Creating new DynamicOutput directory for App ' . $flags['name'][0] . ' at ' . $this->determineAppSubDirPath('DynamicOutput', $flags));
+            mkdir($this->determineAppSubDirPath('DynamicOutput', $flags));
+
+            $this->showMessage('Creating new resources directory for App ' . $flags['name'][0] . ' at ' . $this->determineAppSubDirPath('resources', $flags));
+            mkdir($this->determineAppSubDirPath('resources', $flags));
+
+            $this->showMessage('Creating new Responses directory for App ' . $flags['name'][0] . ' at ' . $this->determineAppSubDirPath('Responses', $flags));
+            mkdir($this->determineAppSubDirPath('Responses', $flags));
+
+            $this->showMessage('Creating new Requests directory for App ' . $flags['name'][0] . ' at ' . $this->determineAppSubDirPath('Requests', $flags));
+            mkdir($this->determineAppSubDirPath('Requests', $flags));
+
+            $this->showMessage($this->creatingDirectoryMessage('OutputComponents', $flags['name'][0], $this->determineAppSubDirPath('OutputComponents', $flags)));
+            mkdir($this->determineAppSubDirPath('OutputComponents', $flags));
+    }
+
+    private function creatingDirectoryMessage(string $dirName, string $appName, string $path) : string
+    {
+        return "Creating new $dirName directory for App $appName at $path";
+    }
+
+    /**
+     * @param array<string, array<int, string>> $flags
+     */
+    private function determineAppSubDirPath(string $name, array $flags) : string
+    {
+        return $this->pathToAppDirectory($flags) . DIRECTORY_SEPARATOR . $name;
     }
 
 }
--- ddms/classes/command/StartServer.php
+++ ddms/classes/command/StartServer.php
@@ -11,7 +11,7 @@ class StartServer extends AbstractCommand implements Command
 
     public function run(UserInterface $userInterface, array $preparedArguments = ['flags' => [], 'options' => []]): bool
     {
-        $this->startServer($preparedArguments);
+        $this->startServer($preparedArguments, $userInterface);
         return true;
     }
 
@@ -19,7 +19,7 @@ class StartServer extends AbstractCommand implements Command
      * @param array{"flags": array<string, array<int, string>>, "options": array<int, string>} $preparedArguments
      * &> /dev/null & xdg-open "http://localhost:${1:-8080}" &>/dev/null & disown
      */
-    private function startServer(array $preparedArguments): void
+    private function startServer(array $preparedArguments, UserInterface $ui): void
     {
         ['flags' => $flags] = $preparedArguments;
         $serverLogPath = escapeshellarg('/tmp/ddms-php-built-in-server.log');
@@ -28,6 +28,12 @@ class StartServer extends AbstractCommand implements Command
         );
         $rootDirectory = escapeshellarg(($flags['root-dir'][0] ?? $this->defaultServerRoot($flags)));
         $domain = escapeshellarg('http://' . str_replace("'", '', $localhost));
+        $ui->showMessage(
+            PHP_EOL .
+            'Starting server @ ' . $domain . ' using ' . $rootDirectory . ' as server root directory.' .
+            PHP_EOL .
+            PHP_EOL
+        );
         shell_exec(
             '/usr/bin/php -S ' . $localhost . ' -t ' . $rootDirectory .
             (isset($flags['php-ini'][0]) ? ' -c ' . escapeshellarg($flags['php-ini'][0]) : '') .
