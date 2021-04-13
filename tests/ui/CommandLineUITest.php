bc970d567d7f093b857349cf586014103a72d8ec
sevidmusic <sdmwwebsdm@gmail.com>
2021-04-13 01:57:13 -0400

    ddms\classes\ui\CommandLineUI: Updated version number shown in banner.

--- tests/ui/CommandLineUITest.php
+++ tests/ui/CommandLineUITest.php
@@ -76,7 +76,7 @@ final class CommandLineUITest extends TestCase
   \e[0m\e[93m __| |__| |_ __  ___\e[0m
   \e[0m\e[94m/ _` / _` | '  \(_-<\e[0m
   \e[0m\e[91m\__,_\__,_|_|_|_/__/\e[0m
-  \e[0m\e[105m\e[30m  v0.0.4-beta  \e[0m\e[0m\e[101m\e[30m  " . date('h:i:s A') . "  \e[0m" . PHP_EOL . PHP_EOL;
+  \e[0m\e[105m\e[30m  v0.0.5-beta  \e[0m\e[0m\e[101m\e[30m  " . date('h:i:s A') . "  \e[0m" . PHP_EOL . PHP_EOL;
     }
 
 }
