57d27778ad42e73040459c91327568332a0cf791
sevidmusic <sdmwebsdm@gmail.com>
2021-05-21 01:03:19 -0400

    ConfigureAppOutput: Continued development of ddms --configure-app-output. Implemented testRunThrowsRuntimeExceptionIfForAppIsNotSpecified(). All PhpUnit tests and PhpUnit --level 8 tests are passing.

--- ddms/classes/command/ConfigureAppOutput.php
+++ ddms/classes/command/ConfigureAppOutput.php
@@ -31,7 +31,11 @@ class ConfigureAppOutput extends AbstractCommand implements Command
             throw new RuntimeException('  You must specify a the name of the App the output is for via the --for-app flag. For help use ddms --help --configure-app-output');
         }
         if(!isset($flags['name'][0])) {
-            throw new RuntimeException('  You must specify a the name of the App the output is for via the --for-app flag. For help use ddms --help --configure-app-output');
+            throw new RuntimeException('  You must specify a name to use for the Components that will be configured for the output via the --name flag. For help use ddms --help --configure-app-output');
         }
+        if(!isset($flags['output'][0]) && !isset($flags['output-source-file'][0])) {
+            throw new RuntimeException('  You must specify either the --output, or --output-source-file flag. For help use ddms --help --configure-app-output');
+        }
+
     }
 }
